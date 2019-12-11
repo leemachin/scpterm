2bc659d34bae71c434c9444a859b71ea1d823223
Lee Machin <me@mrl.ee>
2019-12-11 20:28:11 +0000

    Tweak the game loop

--- src/main.cpp
+++ src/main.cpp
@@ -14,14 +14,14 @@ int main() {
         return 1;
     }
 
-    auto *win = SDL_CreateWindow("scpterm", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    auto *win = SDL_CreateWindow("scpterm", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
     if (win == nullptr) {
         log_sdl_error(std::cout, SDL_GetError());
         SDL_Quit();
         return 1;
     }
 
-    auto *renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_SOFTWARE);
+    auto *renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
     if (renderer == nullptr) {
         log_sdl_error(std::cout, SDL_GetError());
         SDL_DestroyWindow(win);
@@ -29,22 +29,26 @@ int main() {
         return 1;
     }
 
-    SDL_SetRenderDrawColor(renderer, 123, 123, 123, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
-
     SDL_Event evt;
     bool quit = false;
+
     while (!quit) {
-        while (SDL_PollEvent(&evt)) {
+        if (SDL_PollEvent(&evt)) {
             switch(evt.type) {
                 case SDL_QUIT:
                     quit = true;
                     break;
             }
         }
+
+        SDL_SetRenderDrawColor(renderer, rand() % 255, rand() % 255, rand() % 255, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(32);
     }
 
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(win);
     SDL_Quit();
 
