f265dfeecd9d69723d922fd2583c509dbef5a261
Lee Machin <me@mrl.ee>
2019-12-24 20:47:00 +0000

    Render the SCP logo as a texture + setup resources

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
-#include "SDL.h"
+#include <stdio.h>
+#include <string>
+
+#include <SDL.h>
+#include <SDL_image.h>
+
 #include "timer.h"
 
 const int SCREEN_WIDTH = 640;
@@ -11,27 +16,81 @@ void log_sdl_error(std::ostream &os, const std::string &msg) {
     os << msg << " [ERROR] " << SDL_GetError() << std::endl;
 }
 
+#ifdef _WIN32
+const char PATH_SEP = '\\';
+#else
+const char PATH_SEP = '/';
+#endif
 
-int main() {
+SDL_Window* gWin;
+SDL_Renderer* gRenderer;
+
+bool init() {
+    bool success = true;
+
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         log_sdl_error(std::cout, "SDL_Init");
-        return 1;
+        success = false;
     }
 
-    auto *win = SDL_CreateWindow("scpterm", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
-    if (win == nullptr) {
+    gWin = SDL_CreateWindow("scpterm", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
+    if (gWin == nullptr) {
         log_sdl_error(std::cout, "SDL_CreateWindow");
-        SDL_Quit();
-        return 1;
+        success = false;
     }
 
-    auto *renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
-    if (renderer == nullptr) {
-        log_sdl_error(std::cout, "SDL_CreateRenderer");
-        SDL_DestroyWindow(win);
-        SDL_Quit();
-        return 1;
+    int imgFlags = IMG_INIT_PNG;
+    if (!(IMG_Init(imgFlags) & imgFlags)) {
+        log_sdl_error(std::cout, "IMG_Init");
+        success = false;
     }
+
+    gRenderer = SDL_CreateRenderer(gWin, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
+    if (gRenderer == nullptr) {
+        log_sdl_error(std::cout, "SDL_CreateRenderer");
+        success = false;
+    }
+
+    return success;
+}
+
+SDL_Texture* load_texture(std::string path) {
+    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+    SDL_Texture* texture = nullptr;
+
+    if (loadedSurface == nullptr) {
+        // TODO: Better logging of errors
+        std::cout << "Unable to load image " << path.c_str() << "; SDL_image Error: " << IMG_GetError() << std::endl;
+    } else {
+        texture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+        SDL_FreeSurface(loadedSurface);
+    }
+
+    return texture;
+}
+
+std::string get_resource( const std::string &filename, const std::string &subDir = "") {
+    static std::string baseRes;
+
+    if (baseRes.empty()) {
+        char* basePath = SDL_GetBasePath();
+        if (basePath) {
+            baseRes = basePath;
+            SDL_free(basePath);
+        } else {
+            log_sdl_error(std::cout, "scpterm::get_resource_path");
+            return "";
+        }
+
+        size_t pos = baseRes.rfind("bin");
+        baseRes = baseRes.substr(0, pos) + "data" + PATH_SEP;
+    }
+
+    return subDir.empty() ? baseRes + filename:  baseRes + subDir + PATH_SEP + filename;
+}
+
+int main() {
+    init();
 
     SDL_Event evt;
     bool quit = false;
@@ -51,9 +110,17 @@ int main() {
         // update state
 
         // render changes ( pass in lag/MS_PER_UPDATE to balance out rendering)
-        SDL_SetRenderDrawColor(renderer, rand() % 255, rand() % 255, rand() % 255, SDL_ALPHA_OPAQUE);
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
+        auto* logo = load_texture(get_resource("scplogo.png", "img"));
+        if (logo == nullptr) {
+            log_sdl_error(std::cout, "scpterm::load_surface");
+            quit = true;
+            continue;
+        }
+
+        SDL_RenderClear(gRenderer);
+        SDL_SetRenderDrawColor(gRenderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+        SDL_RenderCopy(gRenderer, logo, NULL, NULL);
+        SDL_RenderPresent(gRenderer);
 
         int frameTicks = fpsTimer.get_ticks();
         if (frameTicks < SCREEN_TICKS_PER_FRAME) {
@@ -61,8 +128,8 @@ int main() {
         }
     }
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(win);
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(gWin);
     SDL_Quit();
 
     return 0;
